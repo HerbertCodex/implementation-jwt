3e86887b508ee30e74724b90d0d48f35b95b4f9d
HerbertCodex <krakoffi57@gmail.com>
2025-05-20 19:25:07 +0200

    refactor: remove unused UsersService import from AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
-import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './utils/constants';
@@ -16,7 +15,7 @@ import { jwtConstants } from './utils/constants';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
