ba08682eb1cd79a2b631d02ca95c755a0dacddc1
HerbertCodex <krakoffi57@gmail.com>
2025-05-20 19:35:35 +0200

    feat: add UsersService to AuthModule providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './utils/constants';
@@ -15,7 +16,7 @@ import { jwtConstants } from './utils/constants';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService],
   exports: [AuthService],
 })
 export class AuthModule {}
